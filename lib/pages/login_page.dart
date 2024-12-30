3d3ea93dc8b3552269c3d692b27f4a5050667931
EmirErden <emirerden123@gmail.com>
2024-12-30 23:23:23 +0300

    AddQuesiton is removed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -32,85 +32,6 @@ class _LoginPageState extends State<LoginPage> {
     });
   }
 
-  //Function for adding questions to the firebase. Will be deleted later
-  Future<void> addQuestions() async {
-    try {
-      await db.collection("extQuestions").doc("1").set(Question(
-            id: 1,
-            firstNumber: '15',
-            secondNumber: '  5',
-            options: {'10': true, '11': false, '9': false, '12': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("2").set(Question(
-            id: 2,
-            firstNumber: '18',
-            secondNumber: '  8',
-            options: {'9': false, '10': true, '11': false, '12': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("3").set(Question(
-            id: 3,
-            firstNumber: '20',
-            secondNumber: '  7',
-            options: {'12': false, '13': false, '14': false, '13': true},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("4").set(Question(
-            id: 4,
-            firstNumber: '25',
-            secondNumber: '10',
-            options: {'14': false, '13': false, '15': true, '16': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("5").set(Question(
-            id: 5,
-            firstNumber: '30',
-            secondNumber: '12',
-            options: {'20': false, '19': false, '17': false, '18': true},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("6").set(Question(
-            id: 6,
-            firstNumber: '35',
-            secondNumber: '15',
-            options: {'21': false, '22': false, '20': true, '19': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("7").set(Question(
-            id: 7,
-            firstNumber: '40',
-            secondNumber: '20',
-            options: {'19': false, '20': true, '21': false, '22': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("8").set(Question(
-            id: 8,
-            firstNumber: '50',
-            secondNumber: '25',
-            options: {'27': false, '26': false, '25': true, '24': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("9").set(Question(
-            id: 9,
-            firstNumber: '60',
-            secondNumber: '30',
-            options: {'32': false, '30': true, '29': false, '31': false},
-          ).toMap());
-
-      await db.collection("extQuestions").doc("10").set(Question(
-            id: 10,
-            firstNumber: '70',
-            secondNumber: '35',
-            options: {'34': false, '35': true, '36': false, '37': false},
-          ).toMap());
-
-      print("succesfully addded the question.");
-    } catch (e) {
-      print("Error adding data: $e");
-    }
-  }
-
   Future<void> signIn() async {
     String email = emailTextController.text;
     String password = passwordTextController.text;
@@ -247,8 +168,6 @@ class _LoginPageState extends State<LoginPage> {
                   //sign in button
                   MyButton(onTap: signIn, text: 'Sign In'),
                   const SizedBox(height: 30),
-                  ElevatedButton(
-                      onPressed: addQuestions, child: Text("Add Questions")),
                   const Padding(
                     padding: EdgeInsets.all(4.0),
                     child: Row(
